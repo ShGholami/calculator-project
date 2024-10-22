81e5fd21f32704761f87c5defde1aa4de6b803b0
Shahrbanoo <shahrbanoo.gholami@gmail.com>
2024-10-21 23:16:09 -0600

    Add ValidationUtil

---
+++ src/main/java/com/mycompany/util/ValidationUtil.java
@@ -0,0 +1,22 @@
+package com.mycompany.util;
+
+public class ValidationUtil {
+
+    public static boolean isInteger(String input) {
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static boolean isNumeric(String input) {
+        try {
+            Double.parseDouble(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
