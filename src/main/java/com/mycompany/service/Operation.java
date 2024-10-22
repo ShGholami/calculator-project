41eeb3764aec06a048517d9214d1118dacffd704
Shahrbanoo <shahrbanoo.gholami@gmail.com>
2024-10-21 23:15:03 -0600

    Add IntBinaryOperator to cover situations we have intrger inputs

--- src/main/java/com/mycompany/service/Operation.java
+++ src/main/java/com/mycompany/service/Operation.java
@@ -1,23 +1,30 @@
 package com.mycompany.service;
 
 import java.util.function.DoubleBinaryOperator;
+import java.util.function.IntBinaryOperator;
 
 public enum Operation {
-    PLUS("+", (a, b) -> a + b),
-    MINUS("-", (a, b) -> a - b),
-    TIMES("*", (a, b) -> a * b),
-    DIVIDE("/", (a, b) -> a - b);
+    PLUS("+", (a, b) -> a + b, (a, b) -> a + b),
+    MINUS("-", (a, b) -> a - b, (a, b) -> a - b),
+    TIMES("*", (a, b) -> a * b, (a, b) -> a * b),
+    DIVIDE("/", (a, b) -> a / b, (a, b) -> a / b);
 
     private final String symbol;
-    private final DoubleBinaryOperator operator;
+    private final DoubleBinaryOperator doubleOp;
+    private final IntBinaryOperator intOp;
 
-    Operation(String symbol, DoubleBinaryOperator operator) {
+    Operation(String symbol, DoubleBinaryOperator doubleOp, IntBinaryOperator intOp) {
         this.symbol = symbol;
-        this.operator = operator;
+        this.doubleOp = doubleOp;
+        this.intOp = intOp;
     }
 
     public double apply(double a, double b) {
-        return operator.applyAsDouble(a, b);
+        return doubleOp.applyAsDouble(a, b);
+    }
+
+    public int apply(int a, int b) {
+        return intOp.applyAsInt(a, b);
     }
 
     public String getSymbol() {
